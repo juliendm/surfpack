71affc6393cfa8191379a5759fa5143639caf444
Julien de Muelenaere <juliendm@zion.(none)>
2016-11-01 17:11:01 -0700

    Improve interface

--- src/surfpack_c_interface.cpp
+++ src/surfpack_c_interface.cpp
@@ -8,21 +8,91 @@
 
 #include "surfpack_c_interface.h"
 #include "SurfpackInterface.h"
+#include "SurfData.h"
 #include "SurfpackModel.h"
+#include "ModelFactory.h"
 
 /* Implementation of simplified C interface to some Surfpack library
    functions */
 
 /// one global instance of a SurfpackModel
-static SurfpackModel* surfpackCModel = NULL;
+//typedef std::pair<std::string, SurfData*> SurfDataSymbol;
+typedef std::map<std::string, SurfData*> SurfDataMap;
+//typedef std::pair<std::string, SurfpackModel*> SurfpackModelSymbol;
+typedef std::map<std::string, SurfpackModel*> SurfpackModelMap;
+
+static SurfDataMap dataVars;
+static SurfpackModelMap modelVars;
 
 // Model load example copied from SurfpackInterpreter.cpp; 
 extern "C" 
-int surfpack_load_model(const char * const model_filename)
+int surfpack_load_data(const char * const name, const char * const data_filename, unsigned int n_predictors, unsigned int n_responses, unsigned int n_cols_to_skip)
 {
   bool success = true;
   try {
-    surfpackCModel = SurfpackInterface::LoadModel(std::string(model_filename));
+    SurfData* data = SurfpackInterface::LoadData(std::string(data_filename),n_predictors,n_responses,n_cols_to_skip);
+    //dataVars.insert(SurfDataSymbol(std::string(name), data));
+    if (dataVars.find(std::string(name)) != dataVars.end()) { delete dataVars[std::string(name)]; }
+    dataVars[std::string(name)] = data;
+  }
+  catch (const std::exception& e) {
+    std::cerr << "Error loading surfpack data! Exception:\n" << e.what() 
+        << std::endl;
+    success = false;
+  } 
+  catch (const std::string& s) {
+    std::cerr << "Error loading surfpack data! String:\n" << s << std::endl;
+    success = false;
+  }
+  catch (...) {
+    std::cerr << "Error loading surfpack data! Unknown error." << std::endl;
+    success = false;
+  }
+  return (success ? 0 : 1);
+}
+
+extern "C" 
+int surfpack_save_data(const char * const name, const char * const data_filename)
+{
+  bool success = true;
+  try {
+    SurfData* data = dataVars[std::string(name)];
+    SurfpackInterface::Save(data, std::string(data_filename));
+  }
+  catch (const std::exception& e) {
+    std::cerr << "Error saving surfpack data! Exception:\n" << e.what() 
+        << std::endl;
+    success = false;
+  } 
+  catch (const std::string& s) {
+    std::cerr << "Error saving surfpack data! String:\n" << s << std::endl;
+    success = false;
+  }
+  catch (...) {
+    std::cerr << "Error saving surfpack data! Unknown error." << std::endl;
+    success = false;
+  }
+  return (success ? 0 : 1);
+}
+
+extern "C" 
+void surfpack_add_data(const char * const name, const double * const build_pt, unsigned int num_vars, const double f)
+{
+  std::vector<double> build_vec(build_pt, build_pt + num_vars);
+  if (dataVars.find(std::string(name)) == dataVars.end()) { dataVars[std::string(name)] = new SurfData(); }
+  SurfData* data = dataVars[std::string(name)];
+  data->addPoint(SurfPoint(build_vec,f));
+}
+
+extern "C" 
+int surfpack_load_model(const char * const name, const char * const model_filename)
+{
+  bool success = true;
+  try {
+    SurfpackModel* model = SurfpackInterface::LoadModel(std::string(model_filename));
+    //modelVars.insert(SurfpackModelSymbol(std::string(name), model));
+    if (modelVars.find(std::string(name)) != modelVars.end()) { delete modelVars[std::string(name)]; }
+    modelVars[std::string(name)] = model;
   }
   catch (const std::exception& e) {
     std::cerr << "Error loading surfpack model! Exception:\n" << e.what() 
@@ -40,18 +110,84 @@ int surfpack_load_model(const char * const model_filename)
   return (success ? 0 : 1);
 }
 
-
-extern "C"
-double surfpack_eval_model(const double * const eval_pt, unsigned int num_vars)
+extern "C" 
+int surfpack_save_model(const char * const name, const char * const model_filename)
 {
-  // evaluate the surfpack model, using a std::vector
-  std::vector<double> eval_vec(eval_pt, eval_pt + num_vars);
-  return surfpackCModel->operator()(eval_vec);
+  bool success = true;
+  try {
+    SurfpackModel* model = modelVars[std::string(name)];
+    SurfpackInterface::Save(model, std::string(model_filename));
+  }
+  catch (const std::exception& e) {
+    std::cerr << "Error saving surfpack model! Exception:\n" << e.what() 
+        << std::endl;
+    success = false;
+  } 
+  catch (const std::string& s) {
+    std::cerr << "Error saving surfpack model! String:\n" << s << std::endl;
+    success = false;
+  }
+  catch (...) {
+    std::cerr << "Error saving surfpack model! Unknown error." << std::endl;
+    success = false;
+  }
+  return (success ? 0 : 1);
+}
+
+extern "C" 
+int surfpack_build_model(const char * const name, const char * const type)
+{
+  bool success = true;
+  try {
+    SurfData* data = dataVars[std::string(name)];
+    std::map<std::string, std::string> args;
+    args["type"] = std::string(type);
+    SurfpackModelFactory* smf = ModelFactory::createModelFactory(args);
+    SurfpackModel* model = smf->Build(*data);
+    delete smf;
+    if (modelVars.find(std::string(name)) != modelVars.end()) { delete modelVars[std::string(name)]; }
+    modelVars[std::string(name)] = model;
+
+  }
+  catch (const std::exception& e) {
+    std::cerr << "Error building surfpack model! Exception:\n" << e.what() 
+        << std::endl;
+    success = false;
+  } 
+  catch (const std::string& s) {
+    std::cerr << "Error building surfpack model! String:\n" << s << std::endl;
+    success = false;
+  }
+  catch (...) {
+    std::cerr << "Error building surfpack model! Unknown error." << std::endl;
+    success = false;
+  }
+  return (success ? 0 : 1);
 }
 
 
 extern "C"
+double surfpack_eval_model(const char * const name, const double * const eval_pt, unsigned int num_vars)
+{
+  std::vector<double> eval_vec(eval_pt, eval_pt + num_vars);
+  SurfpackModel* model = modelVars[std::string(name)];
+  return model->operator()(eval_vec);
+}
+
+extern "C"
+double surfpack_variance_model(const char * const name, const double * const eval_pt, unsigned int num_vars)
+{
+  std::vector<double> eval_vec(eval_pt, eval_pt + num_vars);
+  SurfpackModel* model = modelVars[std::string(name)];
+  return model->variance(eval_vec);
+}
+
+extern "C"
 void surfpack_free_model()
 {
-  delete surfpackCModel;
+  for (SurfpackModelMap::iterator iter = modelVars.begin();
+       iter != modelVars.end();
+       ++iter) {
+    delete iter->second; 
+  }
 }
--- src/surfpack_c_interface.h
+++ src/surfpack_c_interface.h
@@ -15,25 +15,58 @@
    program.
 
    Owner: Brian M. Adams, briadam@sandia.gov
-
-   TODO: Give the client to struct for the model, so the client can
-   manage multiple concurrent model instances from C.
 */
 
+/*  */
+#ifdef __cplusplus
+extern "C" 
+#endif
+int surfpack_load_data(const char * const name, const char * const data_filename, unsigned int n_predictors, unsigned int n_responses, unsigned int n_cols_to_skip);
+
+/*  */
+#ifdef __cplusplus
+extern "C" 
+#endif
+int surfpack_save_data(const char * const name, const char * const data_filename);
+
+/*  */
+#ifdef __cplusplus
+extern "C" 
+#endif
+void surfpack_add_data(const char * const name, const double * const build_pt, unsigned int num_vars, const double f);
 
 /* Load a surfpack model from the specified .sps or .bsps file.
    Returns 0 on success, 1 on failure. */
 #ifdef __cplusplus
 extern "C" 
 #endif
-int surfpack_load_model(const char * const model_filename);
+int surfpack_load_model(const char * const name, const char * const model_filename);
+
+/*  */
+#ifdef __cplusplus
+extern "C" 
+#endif
+int surfpack_save_model(const char * const name, const char * const model_filename);
+
+/*  */
+#ifdef __cplusplus
+extern "C" 
+#endif
+int surfpack_build_model(const char * const name, const char * const type);
 
 /* Evaluate the loaded model at the provided eval_pt, with length
    num_vars.  Returns the surrogatem model value at eval_pt. */
 #ifdef __cplusplus
 extern "C"
 #endif
-double surfpack_eval_model(const double * const eval_pt, unsigned int num_vars);
+double surfpack_eval_model(const char * const name, const double * const eval_pt, unsigned int num_vars);
+
+/* Evaluate the loaded model at the provided eval_pt, with length
+   num_vars.  Returns the surrogatem model value at eval_pt. */
+#ifdef __cplusplus
+extern "C"
+#endif
+double surfpack_variance_model(const char * const name, const double * const eval_pt, unsigned int num_vars);
 
 /* Free the surfpack model from memory. */
 #ifdef __cplusplus
